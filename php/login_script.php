1d9e900a71315e096cfd3524a0423cebd7a6d21c
Maksim Buakov <bms1200@gmail.com>
2017-07-15 07:36:01 +0300

    Now it writes to the file not an object of objects but an array of objects.

--- php/login_script.php
+++ php/login_script.php
@@ -5,38 +5,30 @@ $password = $_POST['pass'];
 $_SESSION['user'] = $name;
 $_SESSION['pass'] = $password;
 
-if( !isset($name, $password) || empty($name) || empty($password) ){
+if (!isset($name, $password) || empty($name) || empty($password)){
 	http_response_code(400);
 	die();
 }
-
 $file = "../data/users.json";
 
-$json_array = json_decode(file_get_contents($file), true);
+$json_array= json_decode(file_get_contents($file), true);
 
-$users = array();
+$users = array(); 
 
-if(is_array($json_array) ) {
+if (is_array($json_array)) {
 	$users = $json_array;
 }
-
-if(isset($users[$name]) ) {
-	if( $users[$name]['pass'] !== $password ){
-		http_response_code(400);
+$newUser = true;
+foreach($users as $user) {
+	if ($user["name"] == $name) {
+		$newUser = false;
+		if ($user["pass"] !== $password) {
+			http_response_code(400);
+		} 
 	}
-} 
-else {
-	$users[] = array('name' => $name, 'pass' => $password);
-
-	$new_users = array();
-	$i = 0;
-	foreach($users as $key => $user) {
-		$new_user = $user;
-		$new_user['id'] = $i;
-		$new_users[ $user['name'] ] = $new_user;
-		$i++;
-	}
-
-	file_put_contents($file, json_encode($new_users, JSON_PRETTY_PRINT));
 }
+if ($newUser) {
+	$users[] = array("name" => $name, "pass" => $password, "id" => count($users) + 1);
+}
+file_put_contents($file, json_encode($users, JSON_PRETTY_PRINT));
 ?>
