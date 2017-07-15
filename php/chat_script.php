af6cf7cf4ba4f2e94f559a2fc324d377576a9854
Maksim Buakov <bms1200@gmail.com>
2017-07-15 07:33:09 +0300

    Added id for messages.

--- php/chat_script.php
+++ php/chat_script.php
@@ -4,17 +4,12 @@ $user = $_SESSION['user'];
 $message = $_POST['message'];
 $time = date('U');
 
-$smile = array(":)", ":(");
-$grafic = array("<img src = './image/Smile.png' alt='Smile' align='middle'>",
-	"<img src = './image/Sad.png' alt='Smile' align='middle'>"); 	
-$new_message = str_replace($smile, $grafic, $message);
-
 $file = "../data/messages.json";
 $json_content = json_decode(file_get_contents($file), true);
 
-if (!empty($new_message)) {
-	$json_content[] = array("time" => $time, "user" => $user, "message"  => $new_message);
-	$content =  array("time" => $time, "user" => $user, "message"  => $new_message);
+if (!empty($message)) {
+	$json_content[] = array("time" => $time, "user" => $user, "message"  => $message, "id" => count($json_content) + 1);
+	$content =  array("time" => $time, "user" => $user, "message"  => $message);
 
 	file_put_contents($file, json_encode($json_content, JSON_PRETTY_PRINT));
 }
