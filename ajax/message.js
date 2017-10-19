9614d1d322557bd7bd4cbdea08161a341079411b
omnikross <bms1200@gmail.com>
2017-10-19 14:40:19 +0300

    New func for work with DB and some correct in other

--- ajax/message.js
+++ ajax/message.js
@@ -10,18 +10,29 @@ $(document).ready(function(){
 
 		return string_with_replaced_smile_and_sad;
 	}
+	
+	function data() {
+		$.ajax({
+			type: "POST",
+			url: "php/data.php",
+			dataType : 'json',
+			success: function(someData){
+				callback(someData);
+			},
+			error: function() {
+				alert('Eror!');
+			}
+		});
+	}
 	var exists = [];
-	$.getJSON('data/messages.json', callback);
-
-	function callback(respond) {
+	data();
+	function callback(respond) {	
 		var timeNow = Date.now();
-
 		for (var i = 0; i < respond.length; i++) {
-			var data = respond[i];
+			var data1 = respond[i];
+			if (exists.indexOf(data1[0]) != -1) continue;
 
-			if (exists.indexOf(data.id) != -1) continue;
-
-			var timeInMessage = data.time * 1000;
+			var timeInMessage = data1[1] * 1000;
 			var diff_time = (timeNow - timeInMessage);
 
 			if(diff_time <= 3600000) {
@@ -35,14 +46,14 @@ $(document).ready(function(){
 
 				$('#messages').append(rowClone);
 				$('.time', rowClone).html(res);
-				$('.name', rowClone).html(data.user);
-				$('.message', rowClone).html(smile(data.message));
+				$('.name', rowClone).html(data1[2]);
+				$('.message', rowClone).html(smile(data1[3]));
 				$('.scroller').scrollTop($('#messages').height());
 
-				exists.push(data.id);
+				exists.push(data1[0]);
 			}
 		}
-		setTimeout(function() {$.getJSON('data/messages.json', callback);}, 1000);
+		setTimeout(function(){data();}, 3000);
 	}
 	$('#easyForm').submit(function(){
 		var text = $('#text').val();
@@ -53,12 +64,13 @@ $(document).ready(function(){
 				message:text
 			},
 			success: function() {
-				$.getJSON('data/messages.json', callback);
+				data();
 				$('#text').val('');
 			}
 		});
 		return	false;
 	});
+	
 	$('.logout').click(function(){
 		document.location = 'register.html';
 	});
