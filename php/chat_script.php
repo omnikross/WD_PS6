682ec14df46b57461d615519064bfa7f0346e6ed
omnikross <bms1200@gmail.com>
2017-10-19 14:43:27 +0300

    New script for DB

--- php/chat_script.php
+++ php/chat_script.php
@@ -1,17 +1,16 @@
 <?php
+include('../connection/db.php');
 session_start();
-$user = $_SESSION['user'];
+$name = $_SESSION['user'];
 $message = $_POST['message'];
 $time = date('U');
 
-$file = "../data/messages.json";
-$json_content = json_decode(file_get_contents($file), true);
-
-if (!empty($message)) {
-	$json_content[] = array("time" => $time, "user" => $user, "message"  => $message, "id" => count($json_content) + 1);
-	$content =  array("time" => $time, "user" => $user, "message"  => $message);
-
-	file_put_contents($file, json_encode($json_content, JSON_PRETTY_PRINT));
+if ( !empty($message) ) 
+{
+	mysqli_query($connection, "INSERT INTO `messages` (`time`, `name`, `message`) VALUES ('$time', '$name', '$message')");
 }
+mysqli_close($connection); 
+
+$content =  array("time" => $time, "user" => $user, "message"  => $message);
 echo json_encode($content);
 ?>
